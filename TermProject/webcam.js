213ab0779f22967e8f39f2a6192c91c119006bcb
DatPham123 <d.pham2013@yahoo.com>
2019-04-29 17:22:53 -0500

    changed webcam

--- TermProject/webcam.js
+++ TermProject/webcam.js
@@ -21,33 +21,44 @@ document.querySelector('form').addEventListener('submit', function (ev) {
 p.on('connect', function () {
   console.log('CONNECT')
   p.send('You are connected' + Math.random())
-
-  navigator.getUserMedia({ video: true, audio: true }, gotMedia, function () { })
-
-  function gotMedia(stream) {
-    var peer1 = new Peer({ initiator: true, stream: stream })
-    var peer2 = new Peer({ stream: stream })
-
-    peer1.on('signal', function (data) {
-      peer2.signal(data)
-    })
-
-    peer2.on('signal', function (data) {
-      peer1.signal(data)
-    })
-
-    peer2.on('stream', function (stream) {
-      // got remote video stream, now let's show it in a video tag
-      var video = document.querySelector('video')
-      video.srcObject = stream;
-      video.play()
-    })
-  }
   
 })
 
 p.on('data', function (data) {
   console.log('data: ' + data)
 })
+
+navigator.getUserMedia({ video: true, audio: true }, gotMedia, function () { })
+
+function gotMedia(stream) {
+  var peer1 = new Peer({ initiator: true, stream: stream })
+  var peer2 = new Peer({ stream: stream })
+
+  peer1.on('signal', function (data) {
+    peer2.signal(data)
+  })
+
+  peer2.on('signal', function (data) {
+    peer1.signal(data)
+  })
+
+  peer2.on('stream', function (stream) {
+    // got remote video stream, now let's show it in a video tag
+    var video = document.querySelector('video')
+    video.srcObject = stream;
+    video.play()
+    console.log("video1: ", video.srcObject)
+  })
+
+  peer1.on('stream', function (stream){
+    // got remote video stream, now let's show it in a video tag
+    var video = document.querySelector('video2')
+    video.srcObject = stream;
+    video.play()
+    console.log("video2", video.srcObject)
+  })
+}
+
+
 
 
